4b53673f0ab33814649823a9379e308356e27cd3
Mark Estareja <mark.estareja.me@gmail.com>
2026-04-10 15:15:12 +0800

    Added Responsiveness

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -10,13 +10,13 @@ import { galleryImages } from "@/data/galleryImages";
 export default function Gallery() {
     const [index,setIndex] = useState(-1);
   return (
-    <section className="p-30">
+    <section className="p-6 md:p-15 lg:p-30">
         <div className="space-y-12">
             <div className="w-2/3 space-y-4">
-                <h1 className="text-8xl">Image Gallery</h1>
+                <h1 className="text-4xl md:text-6xl lg:text-8xl font-AmoreChristmas">Image Gallery</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit provident odit itaque optio quos eveniet fugit repudiandae eum sapiente sed labore mollitia, porro dicta aspernatur rerum similique dolor voluptatum numquam?</p>
             </div>
-            <div className="w-full h-auto grid grid-cols-4 gap-4">
+            <div className="w-full h-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {
                     galleryImages.map((galleryImage, i) => (
                         <div className="h-75 w-full relative overflow-hidden rounded-2xl" 
@@ -39,7 +39,7 @@ export default function Gallery() {
                     index={index}
                     slides={galleryImages}
                     carousel={{
-                        imageFit: "cover",
+                        imageFit: "contain",
                     }}
                     styles={{
                         container: {
